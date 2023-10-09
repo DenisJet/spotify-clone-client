cb8b0d84dc4baf7898bc000c08672a32e670a4be
Denis Kalkopf <deniskalkopf@gmail.com>
2023-10-09 17:53:56 +0500

    add track list item styles

--- components/trackItem.tsx
+++ components/trackItem.tsx
@@ -1,4 +1,7 @@
 import { ITrack } from '@/types/track';
+import { Card, Grid, IconButton } from '@mui/material';
+import styles from '../styles/trackItem.module.scss';
+import { Delete, Pause, PlayArrow } from '@mui/icons-material';
 
 interface TrackItemProps {
   track: ITrack;
@@ -6,7 +9,20 @@ interface TrackItemProps {
 }
 
 const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
-  return <div>{track.name}</div>;
+  return (
+    <Card className={styles.track}>
+      <IconButton>{active ? <Pause /> : <PlayArrow />}</IconButton>
+      <img width={70} height={70} src={track.picture} />
+      <Grid container direction='column' style={{ width: 200, margin: '0 20px' }}>
+        <div>{track.name}</div>
+        <div style={{ fontSize: 12, color: 'gray' }}>{track.artist}</div>
+      </Grid>
+      {active && <div>02:42 / 03:22</div>}
+      <IconButton style={{ marginLeft: 'auto' }}>
+        <Delete />
+      </IconButton>
+    </Card>
+  );
 };
 
 export default TrackItem;
